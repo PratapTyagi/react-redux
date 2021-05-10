e1994dca0fd1f22d6b9f000d54b3803453a44a2a
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-10 16:47:10 +0530

    Implemented app fully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increament, decreament } from "./actions/increamentDecreament";
+
 import "./App.css";
 const App = () => {
+  const count = useSelector((state) => state.increamentDecreament);
+  const dispatch = useDispatch();
   return (
     <div className="root">
       <div className="main">
         <h1>Increament Decreament</h1>
         <div className="main_operations">
-          <button className="minus_button">-</button>
-          <div className="value">0</div>
-          <button className="add_button">+</button>
+          <button
+            className="minus_button"
+            onClick={() => dispatch(decreament(1))}
+          >
+            -
+          </button>
+          <div className="value">{count}</div>
+          <button
+            className="add_button"
+            onClick={() => dispatch(increament(1))}
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
